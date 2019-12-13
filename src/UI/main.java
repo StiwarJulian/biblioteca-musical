598f71a932b140949f8a796fc710ef6c32fcfb61
Stiwar <stiwarospina6@gmail.com>
2019-12-13 02:17:56 -0500

    ACTUALIZACION

---
+++ src/UI/main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+/**
+ *
+ * @author Stiwar
+ */
+public class main {
+    
+    public static void main(String[] args) {
+        Principal view = new Principal();
+        view.setVisible(true);
+        
+    }
+}
